cec057075fffd79a5d98634aaebeac4dbbbe50f7
henningvs <henning@vonsee.de>
2015-02-13 23:02:57 +0100

    yaml to json working + first json created

--- genJson.js
+++ genJson.js
@@ -1,16 +1,28 @@
 #!/usr/bin/env node
 
 //Include 'yamljs' module.
-var yaml = require("yamljs");
+var yaml = require("js-yaml");
+var fs = require("fs");
 
-var lang_en_yaml = yaml.load("./openstreetmap-website/config/locales/en.yml");
-var lang_en_json = JSON.stringify(lang_en_yaml);
+var lang_files = fs.readdirSync('./openstreetmap-website/config/locales/');
+console.log('No. of files found in ./openstreetmap-website/config/locales/: ' + lang_files.length);
 
-console.log(lang_en_json);
+//Get all files in the localication dir
+for (var i in lang_files){
+	if(lang_files[i].match(".+?yml")!=null){
+		var current_file = './openstreetmap-website/config/locales/'+lang_files[i];
+		console.log('Convert File: ' + current_file);
+	
+		//Convert the current file
+		var lang_en_yaml = yaml.safeLoad(fs.readFileSync(current_file, 'utf8'));	
+		var lang_en_json = JSON.stringify(lang_en_yaml);
+
+		//Write to file
+		fs.writeFile('./locales/'+lang_files[i].split("\.")[0]+'.json', lang_en_json, 'utf8');
+	}
+}
 
 
-//var lang_en = require("./openstreetmap-website/config/locales/en.yml");
 
-//Log the Englisch json output
-//console.log(JSON.stringify(lang_en, null, "    "));
-//var lang_en_jon = yaml.safeLoad(lang_en);
+
+
