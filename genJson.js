663ba3aa4203f4321b5d44288178de02f37a4afb
henningvs <henning@vonsee.de>
2015-02-13 21:53:34 +0100

    yaml to json working

--- genJson.js
+++ genJson.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 
-//Include 'js-yaml' module.
-var yaml = require("js-yaml");
+//Include 'yamljs' module.
+var yaml = require("yamljs");
 
-var lang_en = require("./openstreetmap-website/config/locales/en.yaml");
+var lang_en_yaml = yaml.load("./openstreetmap-website/config/locales/en.yml");
+var lang_en_json = JSON.stringify(lang_en_yaml);
+
+console.log(lang_en_json);
+
+
+//var lang_en = require("./openstreetmap-website/config/locales/en.yml");
 
 //Log the Englisch json output
-console.log(JSON.stringify(lang_en, null, "    "));
+//console.log(JSON.stringify(lang_en, null, "    "));
+//var lang_en_jon = yaml.safeLoad(lang_en);
