b26192e13bfe5574e24d02d3b0c884d533da2ccc
henningvs <henning@vonsee.de>
2015-02-22 14:33:51 +0100

    moved osm website translations to subfolder

--- osm-website/translations/genLocalesJson.js
+++ osm-website/translations/genLocalesJson.js
@@ -10,7 +10,7 @@ console.log('No. of files found in ./openstreetmap-website/config/locales/: ' + 
 //Get all files in the localication dir
 for (var i in lang_files){
 	if(lang_files[i].match(".+?yml")!=null){
-		var current_file = './openstreetmap-website/config/locales/'+lang_files[i];
+		var current_file = 'openstreetmap-website/config/locales/'+lang_files[i];
 		console.log('Convert File: ' + current_file);
 	
 		//Convert the current file
@@ -18,7 +18,7 @@ for (var i in lang_files){
 		var lang_en_json = JSON.stringify(lang_en_yaml,null,'\t');
 
 		//Write to file
-		fs.writeFile('./locales/'+lang_files[i].split("\.")[0]+'.json', lang_en_json, 'utf8');
+		fs.writeFile('osm-website/translations/locales/'+lang_files[i].split("\.")[0]+'.json', lang_en_json, 'utf8');
 	}
 }
 
