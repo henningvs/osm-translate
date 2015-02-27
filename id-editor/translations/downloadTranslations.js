cc837c1050ab767ad3acc46172b59e93848fe3a6
henningvs <henning@vonsee.de>
2015-02-28 00:18:19 +0100

    fixed download bug

--- id-editor/translations/downloadTranslations.js
+++ id-editor/translations/downloadTranslations.js
@@ -1,26 +1,30 @@
 //Based on https://github.com/openstreetmap/iD/blob/master/data/update_locales.js
 
-var http = require('http');
+var http = require('https');
 var yaml = require("js-yaml");
+var fs = require("fs");
 
 var resources = ['core', 'presets'];
-var outdir = './yaml/';
+var outdir = 'id-editor/translations/yaml/';
 var api = 'https://www.transifex.com/api/2/';
 var project = api + 'project/id-editor/';
-var localesLanguages = {"en", "de"};
+var localesLanguages = ['en', 'de'];
 
-var auth = JSON.parse(fs.readFileSync('./transifex.auth', 'utf8'));
+var auth = JSON.parse(fs.readFileSync('id-editor/translations/transifex.auth', 'utf8'));
 
 getIDTransifexLocales = function() {
 	
 	for(var resource in resources){
 	    for (var currentlanguage in localesLanguages) {
-	    	var currFile = outdir + resource + "_"+currentlanguage+".yaml";
+	    	var currFile = outdir + resources[resource] + "_"+localesLanguages[currentlanguage]+".yaml";
 			console.log('Download id-editor File: ' + currFile);
 	    	var currentLanguageContent = fs.createWriteStream(currFile);
 	    	
+	    	var path = project+'resource/'+resources[resource]+'/translation/'+localesLanguages[currentlanguage]+'/';
+	    	console.log('Get File: '+path);
 	    	var request = http.request({
-	    		'hostname': project+currentlanguage+'/',
+	    		'hostname': path,
+	    		'port': 80,
 	    		'auth': auth["username"] + ':' + auth["password"]
 	    	}, function(response) {
 	    	  response.pipe(currentLanguageContent);
