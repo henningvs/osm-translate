417290242a2935775b0aa51fa093097a3958ec30
Henning von See <hvonsee@gmail.com>
2015-02-28 02:53:13 +0100

    Yaml Downloader is working

--- id-editor/translations/downloadTranslations.js
+++ id-editor/translations/downloadTranslations.js
@@ -3,37 +3,61 @@
 var http = require('https');
 var yaml = require("js-yaml");
 var fs = require("fs");
+var async = require('async');
 
 var resources = ['core', 'presets'];
 var outdir = 'id-editor/translations/yaml/';
-var api = 'https://www.transifex.com/api/2/';
+var host = 'www.transifex.com';
+var api = '/api/2/';
 var project = api + 'project/id-editor/';
 var localesLanguages = ['en', 'de'];
 
 var auth = JSON.parse(fs.readFileSync('id-editor/translations/transifex.auth', 'utf8'));
 
 getIDTransifexLocales = function() {
-	
-	for(var resource in resources){
-	    for (var currentlanguage in localesLanguages) {
-	    	var currFile = outdir + resources[resource] + "_"+localesLanguages[currentlanguage]+".yaml";
-			console.log('Download id-editor File: ' + currFile);
-	    	var currentLanguageContent = fs.createWriteStream(currFile);
-	    	
-	    	var path = project+'resource/'+resources[resource]+'/translation/'+localesLanguages[currentlanguage]+'/';
-	    	console.log('Get File: '+path);
-	    	var request = http.request({
-	    		'hostname': path,
-	    		'port': 80,
-	    		'auth': auth["username"] + ':' + auth["password"]
-	    	}, function(response) {
-	    	  response.pipe(currentLanguageContent);
-	    	});
-	    	
-	    	request.end();
+	var array = [];
+	var count=0;
+	for(var res in resources){
+	    for (var curr in localesLanguages) {
+	    	array[count] = [];
+	    	array[count]['resource'] = resources[res];
+	    	array[count]['language'] = localesLanguages[curr];
+	    	count++;
 	    }		
 	}
+	
+	async.each(array, downloadFile)
 
+};
+
+function downloadFile(reslang, callback){
+	
+	resource = reslang['resource'];
+	language = reslang['language'];
+
+	var currFile = outdir + resource + "_"+language+".yaml";
+	console.log('Download id-editor File: ' + currFile);
+	var currentLanguageContent = fs.createWriteStream(currFile);
+	
+	var path = project+'resource/'+resource+'/translation/'+language+'/';
+	console.log('Get File: '+host+path);
+	
+	var currentRequest = http.get({
+		'hostname': host,
+		'path': path,
+		'port': 443,
+		'method': 'GET',
+		'auth': auth["username"] + ':' + auth["password"]
+		}, function(res) {
+			
+	    res.on('data', function(data) {
+	    		currentLanguageContent.write(data);
+	        }).on('end', function() {
+	        	currentLanguageContent.end();
+	            console.log('Successfully downloaded: '+resource + "_"+language+' to '+outdir);
+	            callback(null);
+	        });
+	});
 };
 
 getIDTransifexLocales();
