7b1eac19458766b51bafa3bfffc44c489d64779a
henningvs <henning@vonsee.de>
2015-08-02 23:33:48 +0200

    json to po converter begonnen

---
+++ translate_id-editor/translations/json2po.js
@@ -0,0 +1,10 @@
+white-space
+#  translator-comments
+#. extracted-comments
+#: reference…
+#, flag…
+#| msgctxt previous-context
+#| msgid previous-untranslated-string
+msgctxt context
+msgid untranslated-string
+msgstr translated-string
